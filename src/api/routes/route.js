467f1014d7e8d8e22c559cebaad05d41a49c0a49
Steef Min <steefmin@gmail.com>
2017-11-22 19:42:17 +0100

    renames routing system

--- src/api/routes/route.js
+++ src/api/routes/route.js
@@ -3,7 +3,7 @@ let router = require('express').Router()
 
 let action = require('../controllers/controller')
 
-router.post('/list', action.list)
+router.post('/api', action.list)
 router.get('/', action.home)
 
 module.exports = router
